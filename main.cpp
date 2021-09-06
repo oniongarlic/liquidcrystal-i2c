f8468cd82efbdb19b8f5b7d61ebc799a0021c26c
Kaj-Michael Lang <milang@tal.org>
2021-09-06 15:03:22 +0300

    Test app adjustments, allow setting i2c adapter number

--- main.cpp
+++ main.cpp
@@ -6,8 +6,13 @@
  **/
 #include "LiquidCrystal_I2C.h"
 
-int main (int argc, char *argv []) {
-// i2c address
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main (int argc, char *argv [])
+{
+// GPIO chip i2c address
 uint8_t i2c=0x27;
 // Control line PINs
 uint8_t en=2;
@@ -23,11 +28,24 @@ uint8_t d7=7;
 // Backlight PIN
 uint8_t bl=3;
 
-// LCD display size
-uint8_t rows=4;
-uint8_t cols=20;
+// LCD display size (1x16, 2x16, 4x20)
+uint8_t rows=2;
+uint8_t cols=16;
 
-LiquidCrystal_I2C lcd("/dev/i2c-1", i2c, en, rw, rs, d4, d5, d6, d7, bl, POSITIVE);
+int adapter_nr=9;
+char filename[20];
+
+if (argc>1) {
+	int a=atoi(argv[1]);
+	if (a>0 && a<9999)
+		adapter_nr=a;
+}
+
+snprintf(filename, 19, "/dev/i2c-%d", adapter_nr);
+
+printf("Using i2c device: %s, Rows: %u, Cols: %u\n", filename, rows, cols);
+
+LiquidCrystal_I2C lcd(filename, i2c, en, rw, rs, d4, d5, d6, d7, bl, POSITIVE);
 
 lcd.begin(cols, rows);
 
@@ -46,6 +64,20 @@ if (rows>2) {
 }
 
 sleep(2);
+
+char hw[]="Hello World";
+
+lcd.clear();
+lcd.cursor();
+lcd.blink();
+for (int i=0;i<strlen(hw);i++) {
+	lcd.write(hw[i]);
+	usleep(20000);
+}
+
+sleep(2);
+lcd.noBlink();
+lcd.noCursor();
 
 lcd.clear();
 lcd.write('H');
@@ -78,7 +110,7 @@ lcd.autoscroll();
 for (uint8_t i=33; i<255; i++) {
 	usleep(35000);
 	lcd.write((char)i);
-	if (i % 8==0)
+	if (i % 16==0)
 		sleep(1);
 }
 sleep(1);
